44fe1b94c195936ae197e306427953f61058ebd1
Olaf Krawczyk <radeko94@gmail.com>
2017-12-01 09:45:25 +0100

    cancel confirm

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,7 +1,7 @@
 import { RestaurantProvider } from './../../providers/restaurant/restaurant';
 import { AuthServiceProvider } from './../../providers/auth-service/auth-service';
 import { Component, OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController  } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -13,7 +13,7 @@ export class AboutPage implements OnInit {
   reservations;
 
   constructor(public navCtrl: NavController, private authService: AuthServiceProvider,
-    private restaurantService: RestaurantProvider) {
+    private restaurantService: RestaurantProvider, public alertCtrl: AlertController) {
 
   }
 
@@ -45,5 +45,28 @@ export class AboutPage implements OnInit {
       },
       error => console.log(error)
     );
+  }
+
+  showConfirm(id) {
+    let confirm = this.alertCtrl.create({
+      title: 'Cancel?',
+      message: 'Cancel selected reservation?',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            console.log('Agree clicked');
+            this.onCancelReservation(id);
+          }
+        }
+      ]
+    });
+    confirm.present();
   }
 }
